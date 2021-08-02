2cca8ca858ad0d0323e94a71a346abb992a15a1b
Kristin Ponsonby <kristinponsonby@gmail.com>
2021-08-02 18:31:59 -0400

    commit tree 8a42d3460c74c500968c2ab9b77cd2360352c0f0

---
+++ index.js
@@ -0,0 +1,64 @@
+
+function wakeDog(dogName, dogBreed) {
+    console.log(`Wake ${dogName} the ${dogBreed}`);
+    return `Wake ${dogName} the ${dogBreed}`
+}
+
+function leashDog(dogName, dogBreed) {
+    console.log(`Leash ${dogName} the ${dogBreed}`);
+    return `Leash ${dogName} the ${dogBreed}`
+}
+
+function walkToPark(dogName, dogBreed) {
+    console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+    return `Walk to the park with ${dogName} the ${dogBreed}`
+}
+
+function throwFrisbee(dogName, dogBreed) {
+    console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+    return `Throw the frisbee for ${dogName} the ${dogBreed}`
+}
+
+function walkHome(dogName, dogBreed) {
+    console.log(`Walk home with ${dogName} the ${dogBreed}`);
+    return `Walk home with ${dogName} the ${dogBreed}`
+}
+
+function unleashDog(dogName, dogBreed) {
+    console.log(`Unleash ${dogName} the ${dogBreed}`);
+    return `Unleash ${dogName} the ${dogBreed}`
+}
+
+var routine = [
+    wakeDog,
+    leashDog,
+    walkToPark,
+    throwFrisbee,
+    walkHome,
+    unleashDog
+]
+    
+function exerciseDog (dogName, dogBreed) {
+    return routine.map(fn => fn(dogName, dogBreed)) 
+}
+
+//function exerciseDog(dog, breed) {
+  //  const exercises = []
+   // for (let i = 0; i < routine.length; i++) {
+     // const fn = routine[i]
+     // const exercise = fn(dog, breed)
+     // exercises.push(exercise)
+   // }
+    //return exercises
+ // }
+  
+  // exerciseDog solution using forEach
+  // function exerciseDog(dog, breed) {
+  //   const exercises = []
+  //   routine.forEach(fn => {
+  //     const exercise = fn(dog, breed)
+  //     exercises.push(exercise)
+  //   })
+  //   return exercises
+  // }
+  
